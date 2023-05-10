0284470fd78973e9ab9dcf3532a0ec942128f314
toadski <toad.frogski@gmail.com>
2023-05-10 23:00:40 +0300

    sessions

--- webapp/src/Repository/SessionRepository.php
+++ webapp/src/Repository/SessionRepository.php
@@ -9,9 +9,11 @@ class SessionRepository
     public static function getAllAvaiableMovieSessions(int $mid) {
         $dbm = DatabaseManager::getInstance();
         $sessions = $dbm->connection->query("
-            SELECT s.*
+            SELECT s.session_id, s.session_time, c.cinema_id
             FROM session s
-            INNER JOIN movie m ON m.movie_id=s.movie_id 
+            INNER JOIN movie m ON m.movie_id=s.movie_id
+            INNER JOIN cinemahall ch ON ch.cinemahall_id=s.cinemahall_id
+            INNER JOIN cinema c ON c.cinema_id=ch.cinema_id
             WHERE m.movie_id={$mid} and s.session_time>'" . date('Y-m-d H:i:s') . "'")
         ->fetch_all();
         return $sessions;
